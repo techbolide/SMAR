973344cc215d5407ef8b7b72a5d33bf5ea36f70c
Mocanu Valentin <rorpvalentin@gmail.com>
2023-11-23 15:44:49 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.techbolide.smar',
+  appName: 'SMAR',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
