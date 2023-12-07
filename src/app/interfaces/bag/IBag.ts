bb658ae16f64562aafb8bf688ce9491b30dbba9e
Mocanu Valentin <rorpvalentin@gmail.com>
2023-12-07 13:10:56 +0200

    feat(bag): seal bag

--- src/app/interfaces/bag/IBag.ts
+++ src/app/interfaces/bag/IBag.ts
@@ -5,3 +5,12 @@ export interface IBag {
     Category: number;
     Date: Date;
 }
+
+export interface ISealBag {
+    Id: number;
+    SealCode: string;
+    PlasticCount: number;
+    AluminiumCount: number;
+    GlassCount: number;
+    ProductCategoryId: number;
+}
--- src/app/pages/bags-seal/bags-seal.page.ts
+++ src/app/pages/bags-seal/bags-seal.page.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ISealBag } from 'src/app/interfaces/bag/IBag';
+import { BagService } from 'src/app/services/bag/bag.service';
+import { ToastService } from 'src/app/services/toast/toast.service';
 
 @Component({
     selector: 'app-bags-seal',
     templateUrl: './bags-seal.page.html',
     styleUrls: ['./bags-seal.page.scss'],
 })
-export class BagsSealPage implements OnInit {
+export class BagsSealPage {
+    public selectedCategory: string | null = null;
 
-    constructor() { }
+    public plasticQuantity: number = 0;
+    public aluminiumQuantity: number = 0;
+    public glassQuantity: number = 0;
 
-    ngOnInit() {
+    public loadingSeal: boolean = false;
+    constructor(private bagService: BagService, private toastService: ToastService, private router: Router) { }
+
+    resetForm() {
+        this.plasticQuantity = 0;
+        this.aluminiumQuantity = 0;
+        this.glassQuantity = 0;
+    }
+
+    hasValidQuantity() {
+        return this.plasticQuantity > 0 || this.aluminiumQuantity > 0 || this.glassQuantity > 0;
+    }
+
+    sealBag() {
+        if(!this.hasValidQuantity || this.loadingSeal || !this.selectedCategory) return;
+
+        this.loadingSeal = true;
+        const model: ISealBag = {
+            Id: -1,
+            SealCode: '571312312',
+            PlasticCount: this.plasticQuantity,
+            AluminiumCount: this.aluminiumQuantity,
+            GlassCount: this.glassQuantity,
+            ProductCategoryId: parseInt(this.selectedCategory, 10)
+        }
+
+        setTimeout(() => {
+
+            this.bagService.sealBag(model).subscribe({
+                next: (res) => {
+                    this.toastService.showToast("Sacul a fost sigilat cu succes!", 2000, 'success', 'bottom');
+                    this.resetForm();
+
+                    setTimeout(() => {
+                        this.router.navigateByUrl('/bag-history', { replaceUrl: true });
+                        this.loadingSeal = false;
+                    }, 500);
+                },
+                error: (err) => {
+                    console.log(err);
+                    this.loadingSeal = false;
+                }
+            });
+        }, 750);
+
     }
 
 }
